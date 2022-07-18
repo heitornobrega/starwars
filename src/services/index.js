1f2c8380b06c974136241c76c3e839898efda47f
heitornobrega <nobheitor@gmail.com>
2022-07-18 09:42:21 -0300

    full coverage tests

--- src/services/index.js
+++ src/services/index.js
@@ -2,6 +2,6 @@ const ENDPOINT = 'https://swapi-trybe.herokuapp.com/api/planets/';
 
 const fetchPlanets = () => fetch(ENDPOINT)
   .then((response) => response.json()
-    .then((json) => (response.ok ? Promise.resolve(json) : Promise.reject(json))));
+    .then((json) => (response.ok && Promise.resolve(json))));
 
 export default fetchPlanets;
